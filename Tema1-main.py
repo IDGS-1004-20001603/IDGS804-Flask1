6d81dfdc09d55f6cf6841eeb8b37be1bceec3eb2
= <=>
2023-02-07 19:55:19 -0600

    Se termino el ejercicio de cinepolis del dia 03/02/2023

--- Tema1-main.py
+++ Tema1-main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return 'Hola mundo'
+    return 'Hola mundo!'
 
 if __name__ == '__main__':
     #Con esto ya no es necesario reiniciar el server
